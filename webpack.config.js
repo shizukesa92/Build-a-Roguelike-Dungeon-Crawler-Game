508469077b9b7087f874280e2e0a637920eed678
Leonard Tye <leonardtye1992@gmail.com>
2018-04-02 06:52:25 +0800

    Installed eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require("webpack");
+const webpack = require("webpack"); /* Needed for jquery */
 
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
+        }
+      },
+	  {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "eslint-loader"
         }
       },
       {
@@ -36,7 +43,7 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css"
     }),
-	new webpack.ProvidePlugin({
+	new webpack.ProvidePlugin({ /* Make sure webpack is defined above */
             $: "jquery",
             jQuery: "jquery"
         })
